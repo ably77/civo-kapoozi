d59c251f3e47b844e79db876cf8887374e3f5333
Alex <alexbrucely@gmail.com>
2022-12-15 16:58:38 -0800

    split collaboration tools overlay into app/config

--- environment/9-collaboration-tools-app/test.sh
+++ environment/9-collaboration-tools-app/test.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-cluster_context="mgmt"
-
 ./tools/wait-for-rollout.sh deployment blog collaboration-tools 10 ${cluster_context}
--- environment/9-collaboration-tools-config/init.sh
+++ environment/9-collaboration-tools-config/init.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "wave description:"
-echo "collaboration tools"
+echo "collaboration tools app config"
